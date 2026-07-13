144e38a950779e2538a848131f67746fc0312724
pywoody-lab <wudrone.project@gmail.com>
2026-07-13 16:53:32 +0800

    class1-1字串及輸出

---
+++ pages/class1-1.py
@@ -0,0 +1,23 @@
+print('Hello world!')
+print('吳定元')
+print('woody-wu')
+print('不要低頭\n雙下巴會出來')
+'''
+這是多行註解
+'''
+#這是單行註解
+print('Hello world!')#print是在終端機顯示文字的指令
+#ctrl+?能快速註解或取消註解
+#基本型態
+print(1)#int是整數,-1,0,1,2
+print(1.0)#float是浮點數
+print(1.234)#float是浮點數
+print('apple')#str是字串"sadasd123125557","1"
+print(True)#bool是布林值,True/False
+print(False)#bool是布林值,True/False
+#變數
+a=10#新增一個新的儲存空間並取名為a,並用'='將左邊的10放入右邊的a中
+print(a)#在終端機顯示a存的值
+a='apple'#將a的直改為'apple'
+print(a)#在終端機顯示a存的值
+
